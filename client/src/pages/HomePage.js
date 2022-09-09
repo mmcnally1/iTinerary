b6db8ff86c76c0352c10fd271d002fb4463e85c2
Joseph Dumont <writedumont@gmail.com>
2022-09-08 19:43:01 -0700

    Add initial Leaflet map

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,10 @@
-import { Map } from '../components/Map.js'
+import Map from '../components/Map'
 
 export default function HomePage() {
   return (
     <>
       <h1>iTinerary</h1>
-      {/* <Map /> */}
+      <Map />
     </>
   )
 }
