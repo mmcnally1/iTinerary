1e046dddf3e5ba89395419e7f3db651988b44428
Joseph Dumont <writedumont@gmail.com>
2022-09-08 20:59:54 -0700

    Allow props passing to maps

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,20 +1,26 @@
 import React, { useEffect } from "react"
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+/**
+ * @param center object with lat, lon array {[0, 0]}
+ * @param zoom object with integer zoom level 0-13
+ * @param marker array of postion, content objects [{position: [0, 0] content: text or jsx}]
+ *
+ * */
+export default function Map({ center, zoom, markers }) {
+  const content = markers.map((item, idx) =>
+    <Marker position={item.position} key={idx}>
+      <Popup>
+        {item.content}
+      </Popup>
+    </Marker>)
 
-export default function Map() {
   return (<MapContainer
-    center={[0, 0]}
-    zoom={1}
-    scrollWheelZoom={false}
-  >
+    center={center}
+    zoom={zoom}
+    scrollWheelZoom={false}>
     <TileLayer
       attribution='&copy; <a href="https:www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https:{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
+      url="https:{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    {content}
   </MapContainer>)
 }
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,13 @@
 import Map from '../components/Map'
 
 export default function HomePage() {
+  const markers = [{ position: [32.715, -117.161], content: <><b>Wecome to San Diego </b><br /> Sister City to Tijuana, Mexico; Yokohama, Japan; and Tema, Ghana </> },
+  { position: [41.882, -87.623], content: <><b>Welcome to Chicago</b> <br /> Home of the Twinkie </> }]
+
   return (
     <>
       <h1>iTinerary</h1>
-      <Map />
+      <Map center={[0, 0]} zoom={1} markers={markers} />
     </>
   )
 }
