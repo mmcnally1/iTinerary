4e5f1592dea773d2c6c5ae0da7351e4c9f3f1cb5
Joseph Dumont <writedumont@gmail.com>
2022-09-10 14:04:29 -0700

    ui for adding location

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import { getUserInfo, getTrips } from '../fetcher.js';
 import TripAdder from '../components/TripAdder.js';
+import PlaceAdder, { LocationMarkers } from '../components/PlaceAdder.js';
 
 import Map from '../components/Map'
 
@@ -60,13 +61,15 @@ export default function UserPage() {
     return (
         <>
             <h1> {userInfo.username} </h1>
-            <Map markers={markers} clickFn={setActiveLocation} />
+            <Map markers={markers} clickFn={setActiveLocation} children={LocationMarkers} />
             {(activeLocation == null)
                 ? <h3>Select a destination from the map.</h3>
                 : <TripSummary info={activeLocation} />}
             <div>
                 <h2>Add a Trip</h2>
                 <TripAdder {...tripProps} />
+                <br />
+                <PlaceAdder {...tripProps} />
             </div>
 
         </>
